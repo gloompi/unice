46b8180668254125f26890ec7379a43e635facb2
Ashirmatov18 <beksultanashirmatov@gmail.com>
2021-08-07 12:12:51 +0600

    login

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config()
+dotenv.config();
 
-export const __filename = fileURLToPath(import.meta.url)
-export const __dirname = path.dirname(__filename)
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
 
 const {
   PORT,
@@ -17,15 +17,15 @@ const {
   GOOGLE_SECRET,
   FACEBOOK_ID,
   FACEBOOK_SECRET,
-} = process.env || {}
+} = process.env || {};
 
 export default {
-  appUrl: 'http://localhost:8080',
+  appUrl: "http://localhost:8080",
   db: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_DB}?retryWrites=true&w=majority`,
-  port: PORT || 8080,
+  port: 8080,
   secret: SECRET,
   googleId: GOOGLE_CLIENT_ID,
   googleSecret: GOOGLE_SECRET,
   facebookId: FACEBOOK_ID,
   facebookSecret: FACEBOOK_SECRET,
-}
+};
