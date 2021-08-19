355f8a0100f4b1879ea9c40cfe74e3c99a457a2b
gloompi <gloompi@gmail.com>
2021-08-19 19:21:55 +0600

    gulp dependencies

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,7 +1,7 @@
 import path from "path";
 import gulp from "gulp";
 import gulpSass from "gulp-sass";
-// import autoprefixer from "gulp-autoprefixer";
+import autoprefixer from "gulp-autoprefixer";
 import cleanCSS from "gulp-clean-css";
 import sassInstance from "sass";
 
@@ -13,10 +13,10 @@ export const buildStyles = () =>
   gulp
     .src(path.resolve(__dirname, "src/*.scss"))
     .pipe(sass().on("error", sass.logError))
-    // .pipe(
-    //   autoprefixer({
-    //     cascade: false,
-    //   })
-    // )
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
     .pipe(cleanCSS())
     .pipe(gulp.dest(path.resolve(__dirname, "public")));
